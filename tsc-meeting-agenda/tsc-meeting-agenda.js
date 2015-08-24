c4896fe56607c9973881fcec19e60b693bce2f52
Rod Vagg <rod@vagg.org>
2015-08-24 15:11:00 +1000

    update for nodejs/node

--- tsc-meeting-agenda/tsc-meeting-agenda.js
+++ tsc-meeting-agenda/tsc-meeting-agenda.js
@@ -4,8 +4,7 @@ const ghauth   = require('ghauth')
 
 const authOptions = { configName: 'iojs-tools' }
     , repos       = [
-          { org: 'nodejs', repo: 'node'  }
-        , { org: 'nodejs', repo: 'io.js' }
+        , { org: 'nodejs', repo: 'node' }
         , { org: 'joyent', repo: 'node'  }
       ]
 
